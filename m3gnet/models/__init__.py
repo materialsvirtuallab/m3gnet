7fcd208e9eb9dc4f1b68c4f87db0b078abc79243
Chi Chen <chc273@eng.ucsd.edu>
2022-03-27 09:19:34 -0700

    add initial model

---
+++ m3gnet/models/__init__.py
@@ -0,0 +1,12 @@
+"""
+Graph models
+"""
+from ._base import Potential, BasePotential, GraphModelMixin
+from ._m3gnet import M3GNet
+
+__all__ = [
+    "GraphModelMixin",
+    "BasePotential",
+    "Potential",
+    "M3GNet"
+]
