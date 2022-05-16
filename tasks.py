d1fbf9bb23b2bb35e3c63ebb405969436ca03faa
Shyue Ping Ong <shyuep@gmail.com>
2022-05-16 13:32:36 -0700

    Add release doc

---
+++ tasks.py
@@ -0,0 +1,115 @@
+"""
+Pyinvoke tasks.py file for automating releases and admin stuff.
+
+Author: Shyue Ping Ong
+"""
+
+
+import glob
+import json
+import os
+import re
+import subprocess
+
+import requests
+from invoke import task
+from monty.os import cd
+
+import m3gnet
+
+NEW_VER = m3gnet.__version__
+
+
+@task
+def make_doc(ctx):
+    with cd("docs_rst"):
+        ctx.run("sphinx-apidoc --separate -d 6 -o . -f ../m3gnet")
+        for f in glob.glob("*.rst"):
+            if f.startswith("m3gnet") and f.endswith("rst"):
+                newoutput = []
+                suboutput = []
+                subpackage = False
+                with open(f) as fid:
+                    for line in fid:
+                        clean = line.strip()
+                        if clean == "Subpackages":
+                            subpackage = True
+                        if not subpackage and not clean.endswith("tests"):
+                            newoutput.append(line)
+                        else:
+                            if not clean.endswith("tests"):
+                                suboutput.append(line)
+                            if clean.startswith("m3gnet") and not clean.endswith("tests"):
+                                newoutput.extend(suboutput)
+                                subpackage = False
+                                suboutput = []
+
+                with open(f, "w") as fid:
+                    fid.write("".join(newoutput))
+        ctx.run("make html")
+
+        # ctx.run("cp _static/* ../docs/html/_static")
+
+    with cd("docs"):
+        ctx.run("cp -r html/* .")
+        ctx.run("rm -r html")
+        ctx.run("rm -r doctrees")
+        ctx.run("rm -r _sources")
+
+        # This makes sure pymatgen.org works to redirect to the Gihub page
+        # ctx.run("echo \"pymatgen.org\" > CNAME")
+        # Avoid the use of jekyll so that _dir works as intended.
+        ctx.run("touch .nojekyll")
+
+
+@task
+def publish(ctx):
+    ctx.run("rm dist/*.*", warn=True)
+    ctx.run("python setup.py sdist bdist_wheel")
+    ctx.run("twine upload dist/*")
+
+
+@task
+def release_github(ctx):
+    with open("CHANGES.md") as f:
+        contents = f.read()
+    toks = re.split(r"\#+", contents)
+    desc = toks[1].strip()
+    payload = {
+        "tag_name": "v" + NEW_VER,
+        "target_commitish": "master",
+        "name": "v" + NEW_VER,
+        "body": desc,
+        "draft": False,
+        "prerelease": False,
+    }
+    response = requests.post(
+        "https://api.github.com/repos/materialsvirtuallab/m3gnet/releases",
+        data=json.dumps(payload),
+        headers={"Authorization": "token " + os.environ["GITHUB_RELEASES_TOKEN"]},
+    )
+    print(response.text)
+
+
+@task
+def update_changelog(ctx):
+    output = subprocess.check_output(["git", "log", "--pretty=format:%s", "v%s..HEAD" % CURRENT_VER])
+    lines = ["* " + l for l in output.decode("utf-8").strip().split("\n")]
+    with open("CHANGES.rst") as f:
+        contents = f.read()
+    l = "=========="
+    toks = contents.split(l)
+    head = "\n\nv%s\n" % NEW_VER + "-" * (len(NEW_VER) + 1) + "\n"
+    toks.insert(-1, head + "\n".join(lines))
+    with open("CHANGES.rst", "w") as f:
+        f.write(toks[0] + l + "".join(toks[1:]))
+    ctx.run("open CHANGES.rst")
+
+
+@task
+def release(ctx, notest=False):
+    ctx.run("rm -r dist build m3gnet.egg-info", warn=True)
+    if not notest:
+        ctx.run("pytest m3gnet")
+    publish(ctx)
+    release_github(ctx)
