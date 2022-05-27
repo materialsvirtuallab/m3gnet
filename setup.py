e0a2177353f14d6e9cc89f33acf58de6697aa223
Shyue Ping Ong <shyuep@gmail.com>
2022-05-27 16:26:08 -0700

    Update reqs.

--- setup.py
+++ setup.py
@@ -70,9 +70,8 @@ setup(
     ],
     classifiers=[
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.5",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
         "Development Status :: 4 - Beta",
         "Intended Audience :: Science/Research",
         "License :: OSI Approved :: BSD License",
