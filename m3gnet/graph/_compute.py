1f255fef071d5ebbcb19648163a7054415e70022
Shyue Ping Ong <sp@ong.ai>
2022-11-16 10:47:41 -0800

    Revert "Refactoring."
    This reverts commit 2dde305a99c3ee8021f9a78cff475bfb51218ab7.

--- m3gnet/graph/_compute.py
+++ m3gnet/graph/_compute.py
@@ -4,62 +4,14 @@ Computing various graph based operations
 
 from typing import List, Optional, Union
 
-import itertools
-
 import numpy as np
 import tensorflow as tf
 
 from m3gnet.config import DataType
 from m3gnet.utils import get_length, get_segment_indices_from_n
 
+from ._threebody_indices import compute_threebody as _compute_threebody
 from ._types import Index, MaterialGraph
-
-
-def _compute_threebody(bond_atom_indices, n_atoms):
-    """
-    Calculate the three body indices from pair atom indices
-
-    Args:
-        bond_atom_indices (np.ndarray): pair atom indices
-        n_atoms (list): number of atoms in each structure.
-
-    Returns:
-        triple_bond_indices (np.ndarray): bond indices that form three-body
-        n_triple_ij (np.ndarray): number of three-body angles for each bond
-        n_triple_i (np.ndarray): number of three-body angles each atom
-        n_triple_s (np.ndarray): number of three-body angles for each structure
-    """
-    n_bond = len(bond_atom_indices)
-    n_struct = len(n_atoms)
-    n_atom = np.sum(n_atoms)
-
-    n_bond_per_atom = [np.sum(bond_atom_indices[:, 0] == i) for i in range(n_atom)]
-
-    n_triple_i = np.zeros(n_atom, dtype=np.int32)
-    n_triple_ij = np.zeros(n_bond, dtype=np.int32)
-    n_triple_s = np.zeros(n_struct, dtype=np.int32)
-    triple_bond_indices = []
-
-    n_triple = 0
-    start = 0
-
-    for i, bpa in enumerate(n_bond_per_atom):
-        n_triple_temp = bpa * (bpa - 1)
-        n_triple_ij[start : start + bpa] = bpa - 1
-        n_triple += n_triple_temp
-        n_triple_i[i] = n_triple_temp
-        for j, k in itertools.permutations(range(bpa), 2):
-            triple_bond_indices.append([start + j, start + k])
-        start += bpa
-
-    start = 0
-    for i, n in enumerate(n_atoms):
-        end = start + n
-        for j in range(start, end):
-            n_triple_s[i] += n_triple_i[j]
-        start = end
-
-    return triple_bond_indices, n_triple_ij, n_triple_i, n_triple_s
 
 
 def get_pair_vector_from_graph(graph: List):
