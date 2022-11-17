9398a3796b4ec371e9831aeafbc0600e7d157dc5
Shyue Ping Ong <sp@ong.ai>
2022-11-17 09:05:05 -0800

    Set up test on py310 and py311.

--- setup.py
+++ setup.py
@@ -64,6 +64,8 @@ setup(
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
         "Development Status :: 4 - Beta",
         "Intended Audience :: Science/Research",
         "License :: OSI Approved :: BSD License",
